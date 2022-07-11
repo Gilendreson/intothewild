3aeee2615fd3203b777d33c4632da074a77a3554
Gilendreson <gilendreson@gmail.com>
2022-07-11 01:22:51 -0300

    Inclusão de animações com JS

--- static/script.js
+++ static/script.js
@@ -22,3 +22,27 @@ navItem.forEach(item => {
         }
     })
 });
+
+const item = document.querySelectorAll("[data-anime]");
+    const animeScroll = () => {
+        const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+        item.forEach((Element) => {
+            if (windowTop > Element.offsetTop) {
+                Element.classList.add("animate");
+            } else {
+                    Element.classList.remove("animate");
+                }
+
+        });           
+    };
+    
+
+ 
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+});
+
